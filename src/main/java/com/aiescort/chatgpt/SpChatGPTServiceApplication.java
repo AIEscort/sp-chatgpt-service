2130ce7869fd696a619f24d057fcfcbfa72bf243
Volodymyr <vzahanych@gmail.com>
2023-10-16 08:59:49 +0200

    generate start application

---
+++ src/main/java/com/aiescort/chatgpt/SpChatGPTServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aiescort.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpChatGPTServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpChatGPTServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiescort/chatgpt/SpChatGPTServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiescort.chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpChatGPTServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
